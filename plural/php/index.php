bc2ebfcf77436252adc61d357206c67be604f57b
robonen <robonenandrew@gmail.com>
2024-04-08 23:09:04 +0700

    refactor(repo): cleanup

--- plural/php/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function plural(int $count, array $words)
-{
-  $cases = [2, 0, 1, 1, 1, 2];
-  return $words[
-    $count % 100 > 4 && $count % 100 < 20 ? 2 : $cases[min($count % 10, 5)]
-  ];
-}

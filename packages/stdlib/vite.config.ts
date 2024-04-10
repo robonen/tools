04347254b458db9685dc10203b799dde5ebecc6a
robonen <robonenandrew@gmail.com>
2024-04-10 16:49:49 +0700

    feat(packages/stdlib): base vite config

--- packages/stdlib/vite.config.ts
+++ packages/stdlib/vite.config.ts
@@ -3,12 +3,22 @@ import dts from 'vite-plugin-dts';
 import { resolve } from 'pathe';
 
 export default defineConfig({
+  build: {
+    lib: {
+      name: 'Stdlib',
+      fileName: 'stdlib',
+      entry: resolve(__dirname, './src/index.ts'),
+    },
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
     },
   },
   plugins: [
-    dts({ insertTypesEntry: true }),
+    dts({
+      insertTypesEntry: true,
+      exclude: '**/*.test.ts',
+    }),
   ],
 });

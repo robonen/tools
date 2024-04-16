f987f722df471e70a53c0f9aec29b67122bc70ad
robonen <robonenandrew@gmail.com>
2024-04-16 16:32:56 +0700

    feat(packages/stdlib): add PubSub class and tests to patterns

---
+++ packages/stdlib/src/patterns/behavioral/pubsub/index.test.ts
@@ -0,0 +1,95 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { PubSub } from './index';
+
+describe('PubSub', () => {
+  let eventBus: PubSub<{
+    event1: (arg: string) => void;
+    event2: () => void
+  }>;
+
+  beforeEach(() => {
+    eventBus = new PubSub();
+  });
+
+  it('add a listener and emit an event', () => {
+    const listener = vi.fn();
+
+    eventBus.on('event1', listener);
+    eventBus.emit('event1', 'Hello');
+
+    expect(listener).toHaveBeenCalledWith('Hello');
+  });
+
+  it('add multiple listeners and emit an event', () => {
+    const listener1 = vi.fn();
+    const listener2 = vi.fn();
+
+    eventBus.on('event1', listener1);
+    eventBus.on('event1', listener2);
+    eventBus.emit('event1', 'Hello');
+
+    expect(listener1).toHaveBeenCalledWith('Hello');
+    expect(listener2).toHaveBeenCalledWith('Hello');
+  });
+
+  it('add a one-time listener and emit an event', () => {
+    const listener = vi.fn();
+
+    eventBus.once('event1', listener);
+    eventBus.emit('event1', 'Hello');
+    eventBus.emit('event1', 'World');
+
+    expect(listener).toHaveBeenCalledWith('Hello');
+    expect(listener).not.toHaveBeenCalledWith('World');
+  });
+
+  it('add once listener and emit multiple events', () => {
+    const listener = vi.fn();
+
+    eventBus.once('event1', listener);
+    eventBus.emit('event1', 'Hello');
+    eventBus.emit('event1', 'World');
+    eventBus.emit('event1', '!');
+
+    expect(listener).toHaveBeenCalledTimes(1);
+    expect(listener).toHaveBeenCalledWith('Hello');
+  });
+
+  it('remove a listener', () => {
+    const listener = vi.fn();
+
+    eventBus.on('event1', listener);
+    eventBus.off('event1', listener);
+    eventBus.emit('event1', 'Hello');
+
+    expect(listener).not.toHaveBeenCalled();
+  });
+
+  it('clear all listeners for an event', () => {
+    const listener1 = vi.fn();
+    const listener2 = vi.fn();
+
+    eventBus.on('event1', listener1);
+    eventBus.on('event1', listener2);
+    eventBus.clear('event1');
+    eventBus.emit('event1', 'Hello');
+
+    expect(listener1).not.toHaveBeenCalled();
+    expect(listener2).not.toHaveBeenCalled();
+  });
+
+  it('return true when emitting an event with listeners', () => {
+    const listener = vi.fn();
+
+    eventBus.on('event1', listener);
+    const result = eventBus.emit('event1', 'Hello');
+
+    expect(result).toBe(true);
+  });
+
+  it('return false when emitting an event without listeners', () => {
+    const result = eventBus.emit('event1', 'Hello');
+
+    expect(result).toBe(false);
+  });
+});

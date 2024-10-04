00fd5846aa6ab871e3f315701ee415ec763a3f23
robonen <robonenandrew@gmail.com>
2024-10-05 05:43:13 +0700

    feat(packages/stdlib): add timestamp and noop utils

---
+++ packages/stdlib/src/utils/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Returns the current timestamp
+ * 
+ * @returns {number} The current timestamp
+ */
+export const timestamp = () => Date.now();
+
+/**
+ * No operation function
+ * 
+ * @returns {void} Nothing
+ */
+export const noop = () => {};

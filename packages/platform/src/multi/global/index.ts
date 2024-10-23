ed76a867e639b00e8fa4fd88b96e71e26a3fa246
robonen <robonenandrew@gmail.com>
2024-10-23 07:16:46 +0700

    chore(packages/platform): prepare for 0.0.1  release

--- packages/platform/src/multi/global/index.ts
+++ packages/platform/src/multi/global/index.ts
@@ -5,7 +5,7 @@
  * @category Multi
  * @description Global object that works in any environment
  *
- * @since 0.0.2
+ * @since 0.0.1
  */
 export const _global =
   typeof globalThis !== 'undefined'
@@ -23,6 +23,6 @@ export const _global =
  * @category Multi
  * @description Check if the current environment is the client
  * 
- * @since 0.0.2
+ * @since 0.0.1
  */
 export const isClient = typeof window !== 'undefined' && typeof document !== 'undefined';

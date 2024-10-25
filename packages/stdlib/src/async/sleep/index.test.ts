3cc500f22b17ced785933ed094ea89ebcce85c35
robonen <robonenandrew@gmail.com>
2024-10-26 06:47:59 +0700

    chore(packages/stdlib): remove unused import

--- packages/stdlib/src/async/sleep/index.test.ts
+++ packages/stdlib/src/async/sleep/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, afterEach, beforeEach } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { sleep } from '.';
 
 describe('sleep', () => {

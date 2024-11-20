979fd6e6dfeb6d2ca37449d68d112e592e907b15
robonen <robonenandrew@gmail.com>
2024-11-20 16:48:47 +0700

    build(packages/platform): setup test suit, add build config

--- packages/platform/build.config.ts
+++ packages/platform/build.config.ts
@@ -1,9 +1,16 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
+  entries: [
+    'src/browsers',
+    'src/multi',
+  ],
+  clean: true,
+  declaration: true,
   rollup: {
+    emitCJS: true,
     esbuild: {
-        // minify: true,
+      // minify: true,
     },
   },
 });

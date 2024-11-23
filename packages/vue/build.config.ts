a89723126eb963c0d2341b0592fb751e5d55bc78
robonen <robonenandrew@gmail.com>
2024-11-23 13:21:09 +0700

    build(packages/vue): enable deps inlining

--- packages/vue/build.config.ts
+++ packages/vue/build.config.ts
@@ -3,6 +3,7 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   externals: ['vue'],
   rollup: {
+    inlineDependencies: true,
     esbuild: {
         // minify: true,
     },

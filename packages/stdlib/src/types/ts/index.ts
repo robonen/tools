90dbdf4c883b20d781cf7830827201bcc77fb4a5
robonen <robonenandrew@gmail.com>
2024-11-30 04:24:49 +0700

    feat(packages/stdlib): add promise types

--- packages/stdlib/src/types/ts/index.ts
+++ packages/stdlib/src/types/ts/index.ts
@@ -1,3 +1,4 @@
-export * from './string';
-export * from './function';
 export * from './array';
+export * from './function';
+export * from  './promise';
+export * from './string';
---
+++ packages/stdlib/src/types/ts/promise.ts
@@ -0,0 +1,4 @@
+/**
+ * Represents a value that may be a promise.
+ */
+export type MaybePromise<T> = T | Promise<T>;

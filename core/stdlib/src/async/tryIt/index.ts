433ab1c2cd32575a034696a913985d8b6916a5de
robonen <robonenandrew@gmail.com>
2026-03-26 06:15:51 +0700

    refactor(core/stdlib): streamline tryIt function with improved promise handling

--- core/stdlib/src/async/tryIt/index.ts
+++ core/stdlib/src/async/tryIt/index.ts
@@ -4,21 +4,24 @@ export type TryItReturn<Return> = Return extends Promise<any>
   ? Promise<{ error: Error; data: undefined } | { error: undefined; data: Awaited<Return> }>
   : { error: Error; data: undefined } | { error: undefined; data: Return };
 
+function onResolve(data: any) { return { error: undefined, data }; }
+function onReject(error: any) { return { error, data: undefined }; }
+
 /**
  * @name tryIt
  * @category Async
  * @description Wraps promise-based code in a try/catch block without forking the control flow
- * 
+ *
  * @param {Function} fn - The function to try
  * @returns {Function} - The function that will return a tuple with the error and the result
- * 
+ *
  * @example
  * const wrappedFetch = tryIt(fetch);
  * const { error, data } = await wrappedFetch('https://jsonplaceholder.typicode.com/todos/1');
- * 
+ *
  * @example
  * const { error, data } = await tryIt(fetch)('https://jsonplaceholder.typicode.com/todos/1');
- * 
+ *
  * @since 0.0.3
  */
 export function tryIt<Args extends any[], Return>(
@@ -29,9 +32,7 @@ export function tryIt<Args extends any[], Return>(
       const result = fn(...args);
 
       if (isPromise(result))
-        return result
-          .then((value) => ({ error: undefined, data: value }))
-          .catch((error) => ({ error, data: undefined })) as TryItReturn<Return>;
+        return result.then(onResolve).catch(onReject) as TryItReturn<Return>;
 
       return { error: undefined, data: result } as TryItReturn<Return>;
     } catch (error) {
